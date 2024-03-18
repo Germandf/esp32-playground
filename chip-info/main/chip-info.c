eafdd33f5aaaeb8d990673bce9c82411d0de3ab9
Germán De Francesco <germandefrancesco@hotmail.com>
2024-03-18 20:47:05 -0300

    Implement chip-info project

--- chip-info/main/chip-info.c
+++ chip-info/main/chip-info.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "esp_chip_info.h"
 
 void app_main(void)
 {
-
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s.\n",
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
 }
